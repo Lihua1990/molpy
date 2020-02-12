20bef0112473277b3ed6a715a3e7bc49f0e5934b
Lihua1990 <denglihualisa@163.com>
2020-02-12 21:33:32 +0100

    ignore file modified

--- molpy/reader.py
+++
@@ -1,8 +0,0 @@
-import os
-
-dirname = os.path.dirname(os.path.abspath(__file__))
-filename = os.path.join(dirname, 'look_and_say.dat')
-
-with open('look_and_say.dat', 'r') as handle:
-    data = handle.read()
-print(data)
